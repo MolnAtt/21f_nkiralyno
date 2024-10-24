8cbf8316f76e4df5a96f8270c5833e324872d2a7
Molnár Attila <molnar.attila@szlgbp.hu>
2024-10-24 13:34:24 +0200

    Add project files.

---
+++ 21f_nkiralyno/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21f_nkiralyno
+{
+	internal class Program
+	{
+		static int[] valasztasok;
+
+		static bool Ütésben_van_korábbiakkal(int i)
+		{
+
+		}
+		static bool Vane(int i = 0)
+		{
+			bool success = i == valasztasok.Length;
+			bool hopeless = Ütésben_van_korábbiakkal(i); // az ELŐZŐ ütésben van valamelyik megelőzővel
+			bool leaf = i == valasztasok.Length;
+
+			if (success)
+				return true;
+			if (hopeless)
+				return false;
+			//if (leaf)
+			//	return true;
+
+			for (int j = 0; j < 8; j++)
+			{
+				Vane(i+1);
+			}
+
+		}
+		static void Main(string[] args)
+		{
+			valasztasok = new int[8];
+
+		}
+	}
+}
