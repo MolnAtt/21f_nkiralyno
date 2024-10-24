8846820bb1b275c7d4b159b31f5cf768a3df247b
Molnár Attila <molnar.attila@szlgbp.hu>
2024-10-24 14:42:15 +0200

    valami rossz

--- 21f_nkiralyno/Program.cs
+++ 21f_nkiralyno/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,31 +13,52 @@ namespace _21f_nkiralyno
 
 		static bool Ütésben_van_korábbiakkal(int i)
 		{
+			if (i==valasztasok.Length)
+			{
+				return false;
+			}
 
+			int j = 0;
+			while (j < i && !( valasztasok[i] == valasztasok[j] || i-j == Math.Abs(valasztasok[i] - valasztasok[j])))
+			{
+				j++;
+			}
+			return j < i;
 		}
 		static bool Vane(int i = 0)
 		{
 			bool success = i == valasztasok.Length;
-			bool hopeless = Ütésben_van_korábbiakkal(i); // az ELŐZŐ ütésben van valamelyik megelőzővel
 			bool leaf = i == valasztasok.Length;
 
 			if (success)
 				return true;
-			if (hopeless)
-				return false;
+			//bool hopeless = Ütésben_van_korábbiakkal(i); // az ELŐZŐ ütésben van valamelyik megelőzővel
+			//if (hopeless)
+			//	return false;
 			//if (leaf)
 			//	return true;
 
-			for (int j = 0; j < 8; j++)
+			// próbálgatás
+			for (int j = 1; j <= 8; j++)
 			{
-				Vane(i+1);
-			}
+				valasztasok[i] = j;
+				if (!Ütésben_van_korábbiakkal(i+1) && Vane(i + 1))
+				{
+					return true;
+				}
 
+			}
+			valasztasok[i] = 0;
+			return false;
 		}
 		static void Main(string[] args)
 		{
 			valasztasok = new int[8];
+			Vane();
 
-		}
+            Console.WriteLine(string.Join(";", valasztasok));
+
+
+        }
 	}
 }
