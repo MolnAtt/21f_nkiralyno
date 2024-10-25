d4439e0af0845d05d7206677177f798c7e9dc0ee
Molnár Attila <molnar.attila@szlgbp.hu>
2024-10-25 16:01:50 +0200

    8kiralynő fix

--- 21f_nkiralyno/Program.cs
+++ 21f_nkiralyno/Program.cs
@@ -13,52 +13,54 @@ namespace _21f_nkiralyno
 
 		static bool Ütésben_van_korábbiakkal(int i)
 		{
-			if (i==valasztasok.Length)
+			if (valasztasok.Length <= i || i < 0)
 			{
 				return false;
 			}
 
 			int j = 0;
-			while (j < i && !( valasztasok[i] == valasztasok[j] || i-j == Math.Abs(valasztasok[i] - valasztasok[j])))
+			while (j < i && !(valasztasok[i] == valasztasok[j] || i - j == Math.Abs(valasztasok[i] - valasztasok[j])))
 			{
 				j++;
 			}
 			return j < i;
 		}
-		static bool Vane(int i = 0)
+		static bool Vane(int i = -1)
 		{
-			bool success = i == valasztasok.Length;
-			bool leaf = i == valasztasok.Length;
-
-			if (success)
-				return true;
-			//bool hopeless = Ütésben_van_korábbiakkal(i); // az ELŐZŐ ütésben van valamelyik megelőzővel
-			//if (hopeless)
-			//	return false;
+			bool hopeless = Ütésben_van_korábbiakkal(i); // az ELŐZŐ ütésben van valamelyik megelőzővel
+			if (hopeless)
+				return false;
 			//if (leaf)
 			//	return true;
 
-			// próbálgatás
+			bool success = i == valasztasok.Length;
+			//bool leaf = i == valasztasok.Length;
+
+			if (success)
+				return true;
+
+
+
+			// PRÓBÁLGATÁS -- REKURZÍV HÍVÁSOK
+
 			for (int j = 1; j <= 8; j++)
 			{
-				valasztasok[i] = j;
-				if (!Ütésben_van_korábbiakkal(i+1) && Vane(i + 1))
+				if (i < 7)
+					valasztasok[i + 1] = j;
+				if (Vane(i + 1))
 				{
 					return true;
 				}
-
 			}
-			valasztasok[i] = 0;
+			if (i < 7)
+				valasztasok[i + 1] = 0;
 			return false;
 		}
 		static void Main(string[] args)
 		{
 			valasztasok = new int[8];
 			Vane();
-
-            Console.WriteLine(string.Join(";", valasztasok));
-
-
-        }
+			Console.WriteLine(string.Join(";", valasztasok));
+		}
 	}
 }
